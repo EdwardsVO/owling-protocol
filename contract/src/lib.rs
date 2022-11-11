2b55ac844964952d7143d2deeca08afce2fe7875
Edward <edwards.svo@gmail.com>
2022-11-10 22:55:16 -0400

    add comments to the lib file and contract initialization methods

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -9,11 +9,21 @@ use near_sdk::{
 };
 use near_sdk::serde_json::json;
 
+//Developement modules
+//Contains all the information about structs 
 mod metadata;
+
+//Contains get and view methods
 mod enumeration;
+
+//Contains all the contract logic 
 mod form;
+
+//Functions called by modules that might not necessarily be inside the contract implementation
+//Helps the contract readability
 mod internal;
 
+//Functions and structs used on the project 
 use crate::metadata::*;
 use crate::internal::*;
 
@@ -21,9 +31,17 @@ use crate::internal::*;
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
+    //Contract owner id
     pub owner_id: AccountId,
+
+    //Storage that save the {Form} struct with the id as key
+    //Dev {Form} in Metadata module
     pub form_by_id: UnorderedMap<U128, Form>,
+
+    //Save an UnorderedSet of forms per user id or creator
     pub forms_by_creator: LookupMap<AccountId, UnorderedSet<Form>>,
+
+    //Contract metadata
     pub metadata:LazyOption<OwlingContractMetadata>,
 }
 
@@ -34,9 +52,11 @@ pub enum StorageKey {
     OwlingMetadata
 }
 
-// Implement the contract structure
+// Initialize the contract structure
 #[near_bindgen]
 impl Contract {
+
+    // Automatically intialize storages calling the method new()
     #[init]
     pub fn new_meta(owner_id: AccountId) -> Self{
         Self::new(
@@ -47,6 +67,7 @@ impl Contract {
         )
     }
 
+    //Initializes the storage through the StorageKey enum
     #[init]
     pub fn new(owner_id: AccountId, metadata: OwlingContractMetadata) -> Self{
         let this: Contract = Self {
